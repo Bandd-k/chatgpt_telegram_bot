e3f2f090bea58863b573d3679fbbd951a0d82b58
Samat Davletshin <samatdav@users.noreply.github.com>
2024-03-06 09:48:29 -0800

    Better reply to error_handle

--- bot/bot.py
+++ bot/bot.py
@@ -777,27 +777,7 @@ async def error_handle(update: Update, context: CallbackContext) -> None:
         "error": str(context.error)
     })
 
-    try:
-        # collect error message
-        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-        tb_string = "".join(tb_list)
-        update_str = update.to_dict() if isinstance(update, Update) else str(update)
-        message = (
-            f"An exception was raised while handling an update\n"
-            f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-            "</pre>\n\n"
-            f"<pre>{html.escape(tb_string)}</pre>"
-        )
-
-        # split text into multiple messages due to 4096 character limit
-        for message_chunk in split_text_into_chunks(message, 4096):
-            try:
-                await context.bot.send_message(update.effective_chat.id, message_chunk, parse_mode=ParseMode.HTML)
-            except telegram.error.BadRequest:
-                # answer has invalid characters, so we send it without parse_mode
-                await context.bot.send_message(update.effective_chat.id, message_chunk)
-    except:
-        await context.bot.send_message(update.effective_chat.id, "Some error in error handler")
+    await context.bot.send_message(update.effective_chat.id, "Ой, произошла ошибка при отправке сообщения, пожалуйста, отправьте заново")
 
 async def post_init(application: Application):
     asyncio.create_task(handle_subgram_events(application.bot))
